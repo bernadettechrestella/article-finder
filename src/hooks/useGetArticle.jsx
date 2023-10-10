839d105b2e84a18a9cd03c06899530275f53a316
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-10 14:34:05 +0900

    Update useGetArticle.jsx

--- src/hooks/useGetArticle.jsx
+++ src/hooks/useGetArticle.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { getAllArticle } from "../services/article.services"
 
-export const useGetArticle =  () => {
+export const useGetArticle =  (options = {}) => {
     const [article, setArticle] = useState([])
 
     useEffect(() => {
@@ -9,6 +9,21 @@ export const useGetArticle =  () => {
             setArticle(data)
         })
     }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getAllArticle((docs) => {
+                    setArticle(docs);
+                }, options);
+                console.log(options)
+          } catch (error) {
+            console.log(error);
+          }
+        };
+    
+        fetchData();
+    }, [options]);
 
     return {article}
 }
