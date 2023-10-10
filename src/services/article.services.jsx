08696510f4365e39a4d1ab99b1e5b37fc4180e05
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-10 14:34:14 +0900

    Update article.services.jsx

--- src/services/article.services.jsx
+++ src/services/article.services.jsx
@@ -1,10 +1,57 @@
 import axios from "axios"
 
-export const getAllArticle = async (callback) => {
+// export const getAllArticle = async (callback) => {
+//     try {
+//         const res = await axios.get(
+//             `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=&api-key=PgzOkI7cz0qoQnidmZwCurfcpaWIYIcl`
+//         )
+//         callback(res.data.response.docs)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+
+export const getAllArticle = async (callback, options = {}) => {
     try {
-        const res = await axios.get(
-            `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=&api-key=PgzOkI7cz0qoQnidmZwCurfcpaWIYIcl`
-        )
+        const { beginDate, endDate, facet, facetFields, facetFilter, fl, fq, page, q, sort } = options
+
+        let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?`
+
+        if (beginDate) {
+            url += `begin_date=${beginDate}&`
+        }
+        if (endDate) {
+            url += `end_date=${endDate}&`
+        }
+        if (facet) {
+            url += `facet=${facet}&`
+        }
+        if (facetFields) {
+            url += `facet_fields=${facetFields}&`
+        }
+        if (facetFilter) {
+            url += `facet_filter=${facetFilter}&`
+        }
+        if (fl) {
+            url += `fl=${fl}&`
+        }
+        if (fq) {
+            url += `fq=${fq}&`
+        }
+        if (page) {
+            url += `page=${page}&`
+        }
+        if (q) {
+            url += `q=${q}&`
+        }
+        if (sort) {
+            url += `sort=${sort}&`
+        }
+
+        url += `api-key=PgzOkI7cz0qoQnidmZwCurfcpaWIYIcl`
+
+        const res = await axios.get(url)
+        console.log(url)
         callback(res.data.response.docs)
     } catch (error) {
         console.log(error)
