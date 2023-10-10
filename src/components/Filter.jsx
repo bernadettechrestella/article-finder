669fdcfcb23f1db7020262142b312a8df1df66c7
bernadettechrestella <bernadettechrestella@gmail.com>
2023-10-10 14:34:02 +0900

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react'
 import { BsFillFilterSquareFill } from 'react-icons/bs';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
-const SearchFilter = () => {
+const Filter = (props) => {
+  const {onFilterChange} = props
   const [popUp, setPopUp] = useState(false)
-
   const togglePopUp = () => {
     setPopUp(!popUp)
+  }
+
+  const [beginDate, setBeginDate] = useState('')
+  const [endDate, setEndDate] = useState('')
+
+  const handleFilterChange = () => {
+    if (endDate === '') {
+      setEndDate(Date())
+    }
+    const formattedBeginDate = beginDate.replace(/-/g, '');
+    const formattedEndDate = endDate.replace(/-/g, '');
+    const filterData = {
+      beginDate: formattedBeginDate,
+      endDate: formattedEndDate,
+    }
+    onFilterChange(filterData)
+  }
+
+  const handleFilterClear = () => {
+    setBeginDate('')
+    setEndDate('')
+    onFilterChange({
+      beginDate: '',
+      endDate: '',
+    })
   }
 
   return (
@@ -15,16 +41,26 @@ const SearchFilter = () => {
       {popUp &&
       <div className='absolute z-50 w-full left-0 laptop:bottom-7 bottom-5'>
         <div className='bg-black text-white rounded-t-3xl p-5'>
-          <p className='text-center font-semibold text-xl border-b-2 border-gray-300 mb-5 pb-3'>Filter</p>
-          <div className='flex justify-between mb-2'>
-            <p className='my-auto'>Date Start</p>
-            <input type="date" className='rounded-xl p-1 text-black'/>
+          <div className='flex justify-between border-b-2 border-gray-300 mb-5 pb-3'>
+            <p className='text-center font-semibold text-xl'>Filter</p>
+            <AiOutlineCloseCircle className='text-white cursor-pointer' size={30} onClick={() => togglePopUp()}/>
           </div>
-          <div className='flex justify-between'>
-            <p className='my-auto'>Date End</p>
-            <input type="date" className='rounded-xl p-1 text-black'/>
+          <div className='grid tablet:grid-cols-2 gap-5'>
+            <div className='flex justify-between'>
+              <p className='my-auto'>Date Start</p>
+              <input type="date" className='rounded-xl p-1 text-black cursor-pointer' onChange={(e) => setBeginDate(e.target.value)} value={beginDate}/>
+            </div>
+            <div className='flex justify-between'>
+              <p className='my-auto'>Date End</p>
+              <input type="date" className='rounded-xl p-1 text-black cursor-pointer' onChange={(e) => setEndDate(e.target.value)} value={endDate}/>
+            </div>
           </div>
-          <button className='text-center font-semibold text-lg w-full mt-5 bg-white text-black rounded-xl'>Apply</button>
+          <div className='flex gap-3'>
+          <button className='text-center font-semibold text-lg w-full mt-5 bg-gray-400 text-white rounded-xl'
+            onClick={() => handleFilterClear()}>Clear</button>
+          <button className='text-center font-semibold text-lg w-full mt-5 bg-white text-black rounded-xl'
+            onClick={() => {handleFilterChange(); togglePopUp()}}>Apply</button>
+          </div>
         </div>
       </div>
       }
@@ -32,4 +68,4 @@ const SearchFilter = () => {
   )
 }
 
-export default SearchFilter
+export default Filter
