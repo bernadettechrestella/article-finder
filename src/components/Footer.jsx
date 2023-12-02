d30e6fe3b5bdc920a735e37c206798bc05398d24
bernadettechrestella <bernadettechrestella@gmail.com>
2023-12-02 14:11:38 +0900

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-center py-1 text-white bg-black absolute bottom-0 w-full tablet:text-sm laptop:text-base mobile:text-xs'>
+    <div className='text-center py-1 text-white bg-black w-full tablet:text-sm laptop:text-base mobile:text-xs'>
         <p>Copyright (c) 2023 The New York Times Company. All Rights Reserved.</p>
     </div>
   )
